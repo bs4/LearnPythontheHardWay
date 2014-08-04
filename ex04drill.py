a864a1d3e5631da7b61b479e45847e713426c17f
Brandon Smith <brandon.j.smith@outlook.com>
2014-08-03 21:27:26 -0700

    exercise 04 drill
    Exercise 04 drill in Zed Shaw's book Learn Python the Hard Way.
    Variables and Math

---
+++ ex04drill.py
@@ -0,0 +1,16 @@
+# Python calculator using variables
+
+boy = 1
+girl = 1
+kids = boy + girl
+mom = 1
+dad = 1
+parents = mom + dad
+total_family = parents + kids
+
+print "K & I were married in 2008."
+print "In 2011 we had", girl, "baby girl named E."
+print "Because we now had", girl, "baby, it made K & I parents."
+print "This brought our household to", girl, "kid and", parents, "parents."
+print "In 2013 we had", boy, "baby boy named N."
+print "Now our family has", total_family, "people in it."
