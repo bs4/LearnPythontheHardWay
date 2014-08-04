32ce0f83a8be78657fffe39b144eb35bee32c86a
Brandon Smith <brandon.j.smith@outlook.com>
2014-08-03 21:25:16 -0700

    exercise 03
    Exercise 03 in Zed Shaw's book Learn Python the Hard Way.
    Numbers and Math

---
+++ ex03.py
@@ -0,0 +1,37 @@
+# The below line gives the title of what I'm doing
+print "I will now count my chickens:"
+# The next two lines show the chickens we will be counting: name and number **The math seems wrong**
+print "Hens", 25.0 + 30 / 6
+print "Roosters", 100.0 - 25 * 3 % 4
+# The below line tells what I'm doing next 
+print "Now I will count the eggs:"
+# The below line is a math equation of some sort **The math seems wrong, again**
+print 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# The below line asks the question if one number is less than another
+print "Is it true that 3.0 + 2 < 5 - 7?"
+# The below line asks a True/False statement, Powershell does the math and kicks back the answer
+print 3.0 + 2 < 5 - 7
+# The below 2 lines print the question and math answer to each side of the "less than" equation one line up 
+print "What is 3 + 2?", 3.0 + 2
+print "What is 5 - 7?", 5.0 - 7
+# The below line gives the aha moment
+print "Oh, thats why it's False."
+# The below line goes further into greater than / less than 
+print "How about some more."
+# The below lines shows the answer to lines 15 and 16 as True / False
+print "Is it greater or equal?", 5.0 >= -2
+print "Is it less or equal?", 5.0 <= -2
+
+# *** ORDER OF OPERATIONS = VERY IMPORTANT ***
+# PEMDAS Parentheses  Exponents  Multiplication  Division  Addition  Subtraction (Please Excuse My Dear Aunt Sally) 
+
+# + plus , does addition
+# - minus , does subtraction
+# / slash , does division
+# * asterisk , does multiplication
+# % percent or modulus , does division BUT the "answer" to the equation is the remaining number ex.- 20 % 3 = 2 , 20 / 3 = 6 and 2 left over
+# "" modulus equation X / Y with J remaining, ex.- 100 / 16 with 4 remaining. The result of % is the J part, or remaining part
+# < less-than , returns True/False based on values (or value of resutling equation) on each side of it
+# > greater-than , returns True/False based on values (or value of resulting equation) on each side of it
+# <= less-than-equal , returns True/False based on values (or value of resulting equation) on each side of it
+# >= greater-than-equal , returns True/False based on values (or value of resulting equation) on each side of it
