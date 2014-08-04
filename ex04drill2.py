77242b9cdb798ac38ac240bb6920015b2e3bf140
Brandon Smith <brandon.j.smith@outlook.com>
2014-08-03 21:28:04 -0700

    exercise 04 drill 2
    Exercise 04 drill 2 in Zed Shaw's book Learn Python the Hard Way.
    Variables and Names

---
+++ ex04drill2.py
@@ -0,0 +1,10 @@
+# Python used a calculator for ex4 drills
+
+i = 4
+j = 3
+x = 13
+
+print "Hey %s there." % "you"
+
+# Please Excuse My Dear Aunt Sally
+# Parentheses Exponents Multiplication Division Addition Subtraction    
