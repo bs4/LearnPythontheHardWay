0c562837e2e8260d8ec43985a03e2f099606d9aa
Brandon Smith <brandon.j.smith@outlook.com>
2014-08-03 21:26:03 -0700

    exercise 03 drill 3
    Exercise 03 drill 03 in Zed Shaw's book Learn Python the Hard Way.
    Numbers and Math

---
+++ ex03drill3.py
@@ -0,0 +1,16 @@
+print
+print
+print "The total number of days from January thru June, 2014:"
+print
+print 31 + 28 + 31 + 30 + 31 + 30
+print
+print "What is the number of days in February divided by the number of days in January?"
+print
+print "28 days / 31 days?", 28 / 31
+print
+print "Wait! Zero isn't correct, it didn't show a fraction..."
+print "Let's try this again."
+print
+print "We need to figure out what a 'floating point' number is."
+print
+print "A floating point number has no fixed number of digits before/after the decimal, the decimal point can float
