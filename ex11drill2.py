7a6cef544961cfb626fdb3375e40ce591bfc3cf9
Brandon Smith <brandon.j.smith@outlook.com>
2014-08-10 07:11:27 -0700

    exercise 11 drill 2
    Exercise 11 drill 2 in Zed Shaw's book Learn Python the Hard Way.
    Another riff on ex11, but with "for" and "else" ...not sure it worked,
    just trying it out :)

---
+++ ex11drill2.py
@@ -0,0 +1,17 @@
+print "Let's figure out your likes and dislikes"
+print "What is your favorite color?",
+color = raw_input()
+print "What is your sign?",
+sign = raw_input()
+print "What is your favorite food?",
+food = raw_input()
+print "What hobby do you have?",
+hobby = raw_input()
+print "Do you like men or women?"
+sex = raw_input()
+
+for [sex = raw_input()]
+    if sex = "men"
+        print "Let's pair you with a woman!"
+else:
+    print "Let's pair you with a man!"
