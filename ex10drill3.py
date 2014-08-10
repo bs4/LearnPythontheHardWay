4a00ad3fb43c89fc787bf4f6b9ddee38c8532bca
Brandon Smith <brandon.j.smith@outlook.com>
2014-08-10 06:12:23 -0700

    exercise 10 drill 3
    Exercise 10 drill 3 in Zed Shaw's book Learn Python the Hard Way.
    My riff on the original ex10, I tried some things and couldn't get some
    things to work...like the \v...

---
+++ ex10drill3.py
@@ -0,0 +1,39 @@
+# The theme of ex10drill3 is Eragon
+
+# Most of ex10drill3 is crap, just me messing around with different things and not being able to understand them
+
+sword = "Brisingr"
+father = "Brom"
+greatest = "\tTHE\tFIRE\tSWORD!\v%s"
+
+main_characters = '''
+The characters I find most intriguing
+\t* Roran
+\t* Eragon
+\t* Arya
+\t* Blodgarm
+'''
+
+print greatest % sword
+print main_characters
+
+print "\vWill this tab me\vVertically"
+
+print "\fWill this feed me the form with a spoon"
+
+print "Feed\nMe\n%s\nRight Now!" % father 
+
+eragon = "2\""
+arya = '5\'4"'
+arya = '5\'4"'
+
+print "Eragon is \%s tall." % eragon
+print "Eragon is \%r tall." % eragon
+print "Arya is %s tall." % arya
+print "Arya is %r tall." % arya
+
+body_part = "\\"below\"
+dominant_arm = "below \"the\" belt"
+punch = "swing arm %s." % body_part
+
+print punch
