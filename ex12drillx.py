436cf345e1d8684c01925a01a10fbe6347d7960a
Brandon Smith <brandon.j.smith@outlook.com>
2014-08-10 13:29:49 -0700

    exercise 12 drill
    Exercise 12 Drill from Zed Shaw's book Learn Python the Hard Way.
    Me riffing on ex12. also some tuple practice!? lol, not sure where that
    came from?

---
+++ ex12drillx.py
@@ -0,0 +1,13 @@
+# python tuple
+
+tup1 = ('physics', 'chemistry', '1997', '2000');
+tup2 = (1, 2, 3, 4, 5, 6, 7);
+
+print "tup1[3,4]: ", tup1[3,4]
+print "tup2[1:5]: ", tup2[1:5]
+
+kids = ('nil', 'emma', 'noah', 'trace')
+years = (2010, 2011, 2012, 2013, 2014, 2015, 2016)
+
+#print "Our kids[1:4]: ", kids[1:4]
+#print "They were born years[1, 3, 5], ", years[1, 3, 5]
