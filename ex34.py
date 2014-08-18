35218df27c0b40e59bc0a806a709f07f5e919c36
Brandon Smith <brandon.j.smith@outlook.com>
2014-08-18 09:13:25 -0700

    exercise 34
    Exercise 34 in Zed Shaw's book Learn Python the Hard Way.
    Accessing Elements of Lists

---
+++ ex34.py
@@ -0,0 +1,37 @@
+# NOTE: The idea behind this exercise is straight forward:
+## ordinal vs. cardinal
+# I had a problem getting this to print in python, I figured it out tho
+
+# see prints below, it's basically "print list[cardinal position]"
+
+#animals = ['bear', 'tiger', 'penguin', 'zebra']
+#bear = animals[0]
+
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+
+
+# the below line returns the number of items in the list
+#print len(animals)
+
+# the below line
+#x = animals(
+
+
+#1. The animal at 1. python, position 1
+print animals[1]
+#2. The 3rd animal. peacock, position 2
+print animals[2]
+#3. The 1st animal. bear, position 0
+print animals[0]
+#4. The animal at 3. kangaroo, position 3
+print animals[3]
+#5. The fifth animal. whale, position 4
+print animals[4]
+#6. The animal at 2. peacock, position 2
+print animals[2]
+#7. The 6th animal. platypus, position 5
+print animals[5]
+#8. The animal at 4. whale, position 4
+print animals[4]
