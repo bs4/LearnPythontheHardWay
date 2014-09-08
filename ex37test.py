887d466b09229eaad1590b8996a3cdfd3b40b9cd
Brandon Smith <brandon.j.smith@outlook.com>
2014-09-08 11:01:47 -0700

    exercise 37
    Exercise 37 in Zed Shaw's book Learn Python the Hard Way.
    Symbol Review.
    NOT COMPLETE
    Switched! .txt is the symbol review and the .py file is me giving
    reminder examples of the symbols i had trouble with

---
+++ ex37test.py
@@ -0,0 +1,87 @@
+## as
+
+#with open('ex37.txt') as x:
+#    print x.read()    
+#x.read()
+
+
+## assert
+
+#x = 0
+#assert x > 0, "x is not zero or negative"
+#assert x % 2 == 0, "x is not an even number"
+
+
+## break
+
+#for letter in 'PYTHON':     # First example
+#    if letter == 'H':   # if False, go to print, if True break, think if/elif/else    
+#        break
+#    print 'Current Letter: ', letter
+
+#var = 10                    # Second example
+#while var > 0:
+#    print "Current variable value: ", var
+#    var = var - 1
+#    if var == 5:
+#        break
+        
+#print "Good bye!"
+
+
+## class
+
+#for letter in 'PYTHON':     # First example
+#    if letter == 'H':
+#        continue
+#    print "Current letter: ", letter
+#    
+#var = 10
+#while var > 0:
+#    var = var - 1 
+#    if var == 5:
+#        continue
+#    print "Current variable value: ", var
+#print "Good bye!"
+
+
+## del
+
+#list = ['a', 'b', 45, 46, 47]
+#print list
+#del list[3] # deletes index position 3 from list
+#print list
+#
+#ages = {'emma': 3, 'noah': 1, 'zeek': 101}
+#print ages
+#del ages['noah'] # deletes entry 'noah' from dictionary
+#print ages
+
+
+## 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
