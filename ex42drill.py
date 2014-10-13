d05f222ff3c3fa0a4b0fcb3fbdca62f76206ee3e
Brandon Smith <brandon.j.smith@outlook.com>
2014-10-13 16:37:38 -0700

    exercise 42
    Exercise 42

---
+++ ex42drill.py
@@ -0,0 +1,102 @@
+## Animal is-a object (yes, sort of confusing) look at the extra credit
+
+class Animal(object):
+
+    pass
+    
+## Dog is-a Animal
+class Dog(Animal):
+
+    print "harry balls"
+    
+    def __init__(self, name):
+        ## Dog has-a name of some sort
+        self.name = name 
+        
+    def baseball(self, pitcher, catcher):
+        self.pitcher = pitcher
+        self.catcher = catcher
+        print "%s throws the ball to %s" % (pitcher, catcher)
+   
+        
+## Cat is-a Animalc
+class Cat(Animal):
+    
+    def __init__(self, name):
+        ## Cat has-a name of some sort
+        self.name = name
+        
+## Person is-a object
+class Person(object):
+    
+    def __init__(self, name):
+        ## Person has-a name of some sort
+        self.name = name
+        
+        ## Person has-a pet of some kind
+        self.pet = None
+        
+## Employee is-a Person
+class Employee(Person):
+    
+    def __init__(self, name, salary):
+        ## Employee has-a name the same as Person
+        super(Employee, self).__init__(name)
+        ## Employee has a salary of son
+        self.salary = salary
+        
+ 
+        
+        
+## Fish is-a object
+class Fish(object):
+    pass
+    
+## Salmon is-a Fish
+class Salmon(Fish):
+    pass
+    
+## Halibut is-a Fish
+class Halibut(Fish):
+    pass
+    
+
+## rover is-a Dog
+rover = Dog("Rover")
+
+
+## satan is-a Cat
+satan = Cat("Satan")
+
+## mary is-a Person
+mary = Person("Mary")
+
+## mary has a pet named satan (see above, we know satan is-a Cat)
+mary.pet = satan
+
+## frank is-a Employee that has-a name "Frank" and has-a salary 120000
+frank = Employee("Frank", 120000)
+
+## frank has-a pet named rover (see above, we know rover is-a Dog)
+frank.pet = rover
+
+## flipper is-a Fish
+flipper = Fish()
+
+## crouse is-a Salmon 
+crouse = Salmon()
+
+## harry is-a Halibut
+harry = Halibut()
+print "harry nuts"
+print Dog.baseball("hank", "don")
+
+
+
+
+
+
+
+
+
+
